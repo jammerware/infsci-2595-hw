02375ca6081945097c582a14669192d31d32ec4c
Ben Stein <ben.s.stein@gmail.com>
2022-04-16 18:18:58 -0400

    i forget

--- final-project/tools.R
+++ final-project/tools.R
@@ -1,21 +1,48 @@
-# load tidyverse
+# load libraries
 library(tidyverse)
+library(recipes)
 
 # enable raw/pre-processed loading of data
-load_project_data <- function(raw = FALSE) {
-  df_raw <- readr::read_csv("./final_project_train.csv", col_names = TRUE)
+load_project_data <- function(info = NULL) {
+  df <- readr::read_csv("./final_project_train.csv", col_names = TRUE)
   
-  if (raw) {
-    return(df_raw)
+  if (is.null(info)) {
+    info = list(
+      convert_data_types = TRUE,
+      response_to_log = TRUE
+    )
   }
   
-  return(
-    df_raw %>% mutate_if(is.character, as.factor)
-  )
+  if (info$convert_data_types == TRUE) {
+    df <- df %>% mutate_if(is.character, as.factor)
+  }
+  
+  if (info$response_to_log == TRUE) {
+    df <- df %>%
+      mutate(
+        response_log = log(response),
+        response = NULL
+      )
+  }
+  
+  # if (preprocess_data) {
+  #   rec_preprocessed <- recipe(
+  #     response ~ .,
+  #     data = df_raw
+  #   ) %>%
+  #     step_normalize(all_numeric_predictors()) %>%
+  #     step_log(all_outcomes())  %>%
+  #     prep()
+  #   
+  #   df_raw <- bake(rec_preprocessed, new_data = df_raw)
+  # }
+  
+  return(df)
 }
 
 # select semantic groups of variables
 bookkeeping_vars <- as.name("rowid")
+
 y_vars <-  list(
   as.name("outcome"),
   as.name("response")
