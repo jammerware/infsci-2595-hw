66314b008f4a7d69cd7b3de812e4485d895e3ccf
Ben Stein <ben.s.stein@gmail.com>
2022-04-20 20:57:25 -0400

    Final nonsense

--- final-project/tools.R
+++ final-project/tools.R
@@ -3,7 +3,12 @@ library(tidyverse)
 library(recipes)
 
 # enable raw/pre-processed loading of data
-load_project_data <- function(convert_categoricals = TRUE, response_to_log = TRUE, outcome_to_numeric = TRUE) {
+load_project_data <- function(
+    convert_categoricals = TRUE, 
+    response_to_log = TRUE, 
+    outcome_to_numeric = TRUE,
+    center_and_scale = FALSE # caret can center/scale with simple arguments, but rstanarm can't
+  ) {
   df <- readr::read_csv("./final_project_train.csv", col_names = TRUE, col_types = cols())
   
   # i'm going to assume that i never want the row_id until further notice
@@ -30,6 +35,11 @@ load_project_data <- function(convert_categoricals = TRUE, response_to_log = TRU
       )
   }
   
+  if (center_and_scale == TRUE) {
+    df <- df %>%
+      mutate(across(is.numeric, ~ as.numeric(scale(.))))
+  }
+  
   # if (preprocess_data) {
   #   rec_preprocessed <- recipe(
   #     response ~ .,
@@ -44,11 +54,6 @@ load_project_data <- function(convert_categoricals = TRUE, response_to_log = TRU
   
   return(df)
 }
-
-y_vars <-  list(
-  as.name("outcome"),
-  as.name("response")
-)
 
 visualize_correlations <- function(data, column_name_start) {
   data %>%
@@ -69,14 +74,76 @@ visualize_distributions <- function(x, column_name_start) {
     theme(axis.text.y = element_blank())
 }
 
-# custom knit to output directory
-knit_to_dir <- function(input) {
-  rmarkdown::render(
-    input,
-    output_file = paste(
-      "./knit/",
-      input
-    ),
-    envir = globalenv()
+visualize_bayesian_coefficients <- function(model) {
+  model %>%
+    plot() +
+    geom_vline(xintercept = 0, color = "grey", linetype = "dashed", size = 1.) +
+    theme_bw()
+}
+
+visualize_bayesian_coefficient_distributions <- function(model) {
+  as.data.frame(model) %>% 
+    tibble::as_tibble() %>% 
+    select(all_of(names(model$coefficients))) %>% 
+    tibble::rowid_to_column("post_id") %>% 
+    pivot_longer(!c("post_id")) %>% 
+    ggplot(mapping = aes(x = value)) +
+    geom_histogram(bins = 55) +
+    facet_wrap(~name, scales = "free", ncol = 3) +
+    theme_bw() +
+    theme(axis.text.y = element_blank())
+}
+
+get_significant_bayesian_coefficients <- function(model) {
+  model %>%
+    posterior_interval %>%
+    as.data.frame %>%
+    rownames_to_column %>%
+    as_tibble %>%
+    filter(!(!!as.symbol("5%")  < 0 & !!as.symbol("95%") > 0)) %>%
+    arrange(desc(abs(!!as.symbol("5%"))))
+}
+
+my_supertrain <- function(formula, data, method, info, parallelize = TRUE, ...) {
+  if (parallelize == TRUE) {
+    parallel_cluster <- makeCluster(detectCores() - 1)
+    registerDoParallel(parallel_cluster) 
+  }
+  
+  out <- tryCatch(
+    {
+      set.seed(2022)
+      
+      model <- train(
+        formula,
+        data = data,
+        method = method,
+        preProcess = c("center", "scale"),
+        metric = info$metric,
+        trControl = info$ctrl,
+        ...
+      )
+      
+      model   
+    },
+    error = function(cond) {
+      message(paste("Error in supertrain:", cond))
+      return(NULL)
+    },
+    finally = {
+      if (exists("parallel_cluster")) {
+        stopCluster(parallel_cluster)
+      }
+    }
   )
+  
+  return(out)
+}
+
+load_model <- function(name) {
+  readr::read_rds(paste("./models/", name, sep = ""))
+}
+
+save_model <- function (name, model, prefix = "") {
+  readr::write_rds(model, paste("./models/", prefix, name ,".rds", sep = ""))
 }
